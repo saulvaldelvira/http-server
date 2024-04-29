e75578ee441138cf0960587861c3fc4d705ca864
Saúl Valdelvira <saul@saulv.es>
2024-04-29 15:34:35 +0200

    Add POST method

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,4 @@
-use std::{env, ffi::OsStr, fmt::Display, fs, io::{BufRead, BufReader, Write}, net::TcpStream, path::Path};
+use std::{collections::HashMap, env, ffi::OsStr, fmt::Display, fs, io::{BufRead, BufReader, Read, Write}, net::TcpStream, path::Path};
 
 pub use super::error::ServerError as HttpError;
 pub type Result<T> = std::result::Result<T,HttpError>;
@@ -29,6 +29,8 @@ impl Display for RequestMethod {
 pub struct Request {
     method: RequestMethod,
     url: String,
+    headers: HashMap<String,String>,
+    data: Vec<u8>,
     version: f32,
     stream: TcpStream,
     status: u16,
@@ -37,18 +39,46 @@ pub struct Request {
 impl Request {
     /* PUBLIC */
     pub fn parse(stream: TcpStream) -> Result<Self>  {
-        let req = BufReader::new(&stream);
-        let mut lines = req.lines();
-        let line = lines.next().unwrap().unwrap();
-        let mut space = line.split_whitespace().take(3);
+        let mut reader = BufReader::new(&stream);
+        let mut line = String::new();
 
+        /* Parse request line */
+        reader.read_line(&mut line)?;
+        let mut space = line.split_whitespace().take(3);
         let method = RequestMethod::from_str(space.next().unwrap())?;
         let url = space.next().unwrap().to_owned();
         let version: f32 = space.next().unwrap()
                                .replace("HTTP/", "")
                                .parse()
                                .or_else(|_| HttpError::from_str("Could not parse HTTP Version").err())?;
-        Ok(Self { method, url, version, stream, status:200 })
+        line.clear();
+        /* Parse Headers */
+        let mut headers = HashMap::new();
+        while let Ok(_) = reader.read_line(&mut line) {
+            if line == "\r\n" { break; }
+            let mut splt = line.split(":");
+            let key = splt.next().unwrap_or("").to_string();
+            let value = splt.next().unwrap_or("")
+                            .strip_prefix(" ").unwrap_or("")
+                            .strip_suffix("\r\n").unwrap_or("")
+                            .to_string();
+            headers.insert(key, value);
+            line.clear();
+        }
+
+        /* Get body (if present) */
+        let len =
+            match headers.get("Content-Length") {
+                Some(l) => l.parse().unwrap_or(0),
+                None => 0,
+            };
+        let mut data:Vec<u8> = Vec::new();
+        if len > 0 {
+            data.resize(len, 0);
+            reader.read_exact(&mut data)?;
+        };
+
+        Ok(Self { method, url, headers, data, version, stream, status:200 })
     }
     pub fn url(&self) -> &str { &self.url }
     pub fn filename(&self) -> Result<String> {
@@ -67,6 +97,7 @@ impl Request {
     pub fn process(&mut self) -> Result<()> {
         match self.method {
             RequestMethod::GET => self.get(),
+            RequestMethod::POST => self.post(),
             _ => self.not_implemented(),
         }
     }
@@ -79,6 +110,15 @@ impl Request {
             501 => "NOT IMPLEMENTED",
             _ => "?"
         }
+    }
+    pub fn content_length(&self) -> usize {
+        match self.headers.get("Content-Length") {
+            Some(l) => l.parse().unwrap_or(0),
+            None => 0,
+        }
+    }
+    pub fn header(&self, key: &str) -> Option<&String> {
+        self.headers.get(key)
     }
     /* PRIVATE */
     fn respond(&mut self, buf: &[u8]) -> Result<()> {
@@ -100,6 +140,11 @@ impl Request {
             self.error_page()
         });
         self.respond(&contents)?;
+        Ok(())
+    }
+    fn post(&mut self) -> Result<()> {
+        fs::write(self.filename()?, &self.data)?;
+        self.respond(&[])?;
         Ok(())
     }
     fn error_page(&mut self) -> Vec<u8> {
